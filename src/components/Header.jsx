f9849ce57941c0ee71c95d1815505ccad1612912
João Martins <joaovmf4@gmail.com>
2024-08-09 02:27:26 +0000

    primeiro commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <ul>
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Como Usar</a>
+            </li>
+            <li>
+                <a href="">Materiais</a>
+            </li>
+            <li>
+                <a href="">Referências</a>
+            </li>
+            <li>
+                <a href="">Sobre nós</a>
+            </li>
+        </ul>
+    </header>
+  )
+}
