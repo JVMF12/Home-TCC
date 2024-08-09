fdd35f9784299b1059f73c32556bdd26a5805ccd
João Martins <joaovmf4@gmail.com>
2024-08-09 02:59:57 +0000

    esqueleto pronto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Logo from '/LOGO.svg'
 
 export default function Header() {
   return (
-    <header>
-        <ul>
+    <header className='flex'>
+        <button className=''>
+            <img src={Logo} alt="" />
+        </button>
+
+        <ul className='flex pl-5'>
             <li>
                 <a href="">Home</a>
             </li>
